387e248008a09e34ef81ee5e911b79eb938da404
Philippe MILINK <philippe.milink@gmx.fr>
2022-03-25 17:59:34 +0100

    Passe par l'utilisateur zds-prod pour purger les sauvegardes
    Il faut que ce soit toujours le même utilisateur qui modifie
    les dépôts Borg.

--- roles/backup/files/beta/cleaning.sh
+++ roles/backup/files/beta/cleaning.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # We keep the data backups for last 60 days and remove the rest
-borg prune --keep-within 60d --list /opt/sauvegarde/data/
-borg prune --keep-within 60d --list /opt/sauvegarde/db-borg/
+sudo -u zds-prod /usr/local/bin/borg prune --keep-within 60d --list --stats /opt/sauvegarde/data/
+sudo -u zds-prod /usr/local/bin/borg prune --keep-within 60d --list --stats /opt/sauvegarde/db-borg/
