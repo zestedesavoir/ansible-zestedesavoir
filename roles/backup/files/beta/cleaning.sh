dd4af18dc81693a95c1f40567e7b3e007c58b020
Philippe MILINK <philippe.milink@gmx.fr>
2022-07-17 18:02:03 +0200

    Corrige le script pour pruner les sauvegardes de Matomo

--- roles/backup/files/beta/cleaning.sh
+++ roles/backup/files/beta/cleaning.sh
@@ -9,8 +9,8 @@ echo "** data **"
 sudo -u zds-prod /usr/local/bin/borg prune --keep-within 60d --list --stats /opt/sauvegarde/data/
 echo "** db **"
 sudo -u zds-prod /usr/local/bin/borg prune --keep-within 60d --list --stats /opt/sauvegarde/db-borg/
-sudo "** matomo **"
-sudo -u zds-matomo /usr/local/bin/borg prune -m 2 --keep-within 10d --list -stats /opt/sauvegarde/matomo
+echo "** matomo **"
+sudo -u zds-matomo /usr/local/bin/borg prune -m 2 --keep-within 10d --list --stats /opt/sauvegarde/matomo/
 
 curl -s -m 10 --retry 5 $(cat /root/healthchecks-backup-cleaning-url)
 echo # to make a newline after the "OK" written by curl
