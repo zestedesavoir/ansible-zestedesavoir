fef2c31c06be1f223fd731b9ea3fb852e40e22b5
Philippe MILINK <philippe.milink@gmx.fr>
2022-09-03 23:35:39 +0200

    Réduit le nombre des sauvegardes conservées

--- roles/backup/files/beta/cleaning.sh
+++ roles/backup/files/beta/cleaning.sh
@@ -4,12 +4,12 @@ set -e
 
 echo "Starting script ($(date))"
 
-# We keep the data backups for last 60 days and remove the rest
-echo "** data **"
-sudo -u zds-prod /usr/local/bin/borg prune --keep-within 60d --list --stats /opt/sauvegarde/data/
-echo "** db **"
-sudo -u zds-prod /usr/local/bin/borg prune --keep-within 60d --list --stats /opt/sauvegarde/db-borg/
-echo "** matomo **"
+DATA_DB_RULES="--keep-within 30d -w 8 -m 3"
+echo "** data ** ($(date))"
+sudo -u zds-prod /usr/local/bin/borg prune $DATA_DB_RULES --list --stats /opt/sauvegarde/data/
+echo "** db ** ($(date))"
+sudo -u zds-prod /usr/local/bin/borg prune $DATA_DB_RULES --list --stats /opt/sauvegarde/db-borg/
+echo "** matomo ** ($(date))"
 sudo -u zds-matomo /usr/local/bin/borg prune -m 2 --keep-within 10d --list --stats /opt/sauvegarde/matomo/
 
 curl -s -m 10 --retry 5 $(cat /root/healthchecks-backup-cleaning-url)
